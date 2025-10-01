676fa7d7ad48d4bfae822ae749947916512d1ac2
kui8shi <gbiifvrc517@gmail.com>
2025-10-01 14:35:18 +0900

    removed AutoconfPool

--- examples/parsing_autoconf.rs
+++ examples/parsing_autoconf.rs
@@ -1,4 +1,3 @@
-use autotools_parser::ast::node::{AutoconfPool, DisplayNode};
 use autotools_parser::lexer::Lexer;
 use autotools_parser::parse::autoconf::NodeParser;
 use std::error::Error;
@@ -11,11 +10,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Initialize our token lexer and shell parser with the program's input
     let lex = Lexer::new(input.chars());
     let (nodes, top_ids) = NodeParser::<_, ()>::new(lex).parse_all();
-    let pool = AutoconfPool::new(nodes, None);
 
     for id in top_ids {
         println!("============================");
-        println!("{}", pool.display_node(id, 0));
+        println!("{:?}", nodes.get(id));
     }
 
     Ok(())
